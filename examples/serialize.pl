14c1a2d810c368d3758a34a5d8e3e73f2361f43f
pmakholm <peter@makholm.net>
2009-10-30 11:25:42 +0100

    Split into module and script

---
+++ examples/serialize.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Benchmark::Serialize;
+
+my @benchmark          = ();      # package names of benchmarks to run
+my $iterations         = -1;      # integer
+my $structure          = {
+    array  => [ 'a' .. 'j' ],
+    hash   => { 'a' .. 'z' },
+    string => 'x' x 200
+};
+
+Getopt::Long::Configure( 'bundling' );
+Getopt::Long::GetOptions(
+    'b|benchmark=s@' => \@benchmark,
+    'deflate!'       => \$Benchmark::Serialize::benchmark_deflate,
+    'inflate!'       => \$Benchmark::Serialize::benchmark_inflate,
+    'i|iterations=i' => \$iterations,
+    'o|output=s'     => \$Benchmark::Serialize::output,
+    's|structure=s'  => sub {
+        die "Structure option requires YAML.\n"
+        unless YAML->require;
+
+        $structure = YAML::LoadFile( $_[1] );
+    }
+) or exit 1;
+
+@benchmark = ("all") unless @benchmark;
+
+Benchmark::Serialize::cmpthese($iterations, $structure, @benchmark);
+
--- lib/Benchmark/Serialize.pm
+++ lib/Benchmark/Serialize.pm
@@ -102,36 +102,6 @@ our $benchmark_inflate  = 1;       # boolean
 our $benchmark_size     = 1;       # boolean
 our $output             = 'chart'; # chart or time
 
-unless( caller() ) {
-    my @benchmark          = ();      # package names of benchmarks to run
-    my $iterations         = -1;      # integer
-    my $structure          = {
-        array  => [ 'a' .. 'j' ],
-        hash   => { 'a' .. 'z' },
-        string => 'x' x 200
-    };
-
-    Getopt::Long::Configure( 'bundling' );
-    Getopt::Long::GetOptions(
-        'b|benchmark=s@' => \@benchmark,
-        'deflate!'       => \$benchmark_deflate,
-        'inflate!'       => \$benchmark_inflate,
-        'i|iterations=i' => \$iterations,
-        'o|output=s'     => \$output,
-        's|structure=s'  => sub {
-            die "Structure option requires YAML.\n"
-            unless YAML->require;
-
-            $structure = YAML::LoadFile( $_[1] );
-        }
-    ) or exit 1;
-
-    @benchmark = grep { $benchmarks->{ $_ }->{default} } keys %{ $benchmarks }
-    unless @benchmark;
-
-    cmpthese($iterations, $structure, @benchmark);
-}
-
 sub cmpthese {
     my $iterations = shift;
     my $structure  = shift;
