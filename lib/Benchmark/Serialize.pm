e551ed19aad992f66a750b3f94f30996a178fde3
pmakholm <peter@makholm.net>
2009-11-02 09:56:52 +0100

    Releasing as 0.02

--- lib/Benchmark/Serialize.pm
+++ lib/Benchmark/Serialize.pm
@@ -9,11 +9,11 @@ Benchmark::Serialize - Benchmarks of serialization modules
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
