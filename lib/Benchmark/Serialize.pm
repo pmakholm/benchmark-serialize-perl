f48c38fd4fbaace06ee11982c3797fc7d3904f39
pmakholm <peter@makholm.net>
2010-09-09 09:24:13 +0200

    Updated version

--- lib/Benchmark/Serialize.pm
+++ lib/Benchmark/Serialize.pm
@@ -9,11 +9,11 @@ Benchmark::Serialize - Benchmarks of serialization modules
 
 =head1 VERSION
 
-Version 0.06
+Version 0.07
 
 =cut
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 =head1 SYNOPSIS
 
